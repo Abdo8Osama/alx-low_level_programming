eb1434c0f4a205a98d192f87baec8e400008069c
Abdo8Osama <abdo.osama301@yahoo.com>
2022-09-14 04:07:15 +0200

    commit 1

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,15 +10,16 @@
  */
 int main(void)
 {
-   unsigned long int t0 = 0, t1 = 1, s;
-    int i;
-    for (i = 1; i<= 98; i++)
-    {
-        s = (unsigned long int)t0 + (unsigned long int)t1;
-        if (i != 98)
-        {
+	unsigned long int t0 = 0, t1 = 1, s;
+	int i;
+
+	for (i = 1; i <= 98; i++)
+	{
+		s = (unsigned long int)t0 + (unsigned long int)t1;
+		if (i != 98)
+		{
 			printf("%lu, ", s);
-        }
+		}
         else
         {
             printf("%lu\n", s);
