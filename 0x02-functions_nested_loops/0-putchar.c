4899e2796581d5b2c13a50d2cf03859e7a4b2f43
Abdo8Osama <abdo.osama301@yahoo.com>
2022-09-14 03:38:20 +0200

    commit 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,14 @@
 #include <unistd.h>
 #include "main.h"
 
- /**
+/**
  * main - Entry point
+ * 
  * Description: 'the program's description'
+ * 
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	_putchar('_');
