16e8e2395afce304e005613a852e84f4c3457681
Abdo8Osama <abdo.osama301@yahoo.com>
2022-09-16 01:36:44 +0200

    commit 1

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -26,12 +26,9 @@ void print_number(int n)
  */
 void print_integer(int m)
 {
-	int i = 1000000;
-
-	for (i = 1000000; i >= 1; i /= 10)
-		if (m / i != 0)
-		{
-			_putchar((m / i) % 10 + '0');
-			m = m % 10 ;
-		}
+	while (m != 0)
+	{
+		_putchar((m % 10) + '0');
+		m = m / 10 ;
+	}
 }
