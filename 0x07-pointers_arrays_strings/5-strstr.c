c4eeaa458fee0d23fa67c32631ddef083f7aa506
Abdo8Osama <abdo.osama301@yahoo.com>
2022-09-27 03:33:24 +0200

    add c files

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,9 +8,9 @@
  * Return:  a pointer to the beginning of the located substring,
  * or NULL if the substring is not found.
  */
-char *_strstr(char *haystack, char *needle)
+char *_strstr(char *haystack , char *needle)
 {
-	char *startn = needle, *starth = haystack;
+	char *startn = needle , *starth = haystack;
 
 	while (*haystack)
 	{
