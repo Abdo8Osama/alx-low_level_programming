d516f0fa09d7f5033d31ddb84bf06200f9598b8d
Abdo8Osama <abdo.osama301@yahoo.com>
2022-10-26 02:57:11 +0200

    chore : update files.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new_node->n = n;
 		return (new_node);
 	}
-
 	if (idx == 0)
 	{
 		new_node->next = *head;
@@ -34,14 +33,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-
 	temp = *head;
 	while (i < idx)
 	{
 		temp = temp->next;
 		i++;
 	}
-
 	new_node->n = n;
 	new_node->next = temp->next;
 	temp->next = new_node;
