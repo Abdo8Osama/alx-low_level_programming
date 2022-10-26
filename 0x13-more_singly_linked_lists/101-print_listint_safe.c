e614f9919dc027d954f79d1188a707f0301b2d57
Abdo8Osama <abdo.osama301@yahoo.com>
2022-10-26 02:41:02 +0200

    chore : update files.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -4,6 +4,7 @@
 /**
  * print_listint_safe - A function that prints the elementsin a  list
  * @head: A pointer to listint_t structure
+ *
  * Return: The number of nodes. Exits with 98 on failure
  */
 size_t print_listint_safe(const listint_t *head)
