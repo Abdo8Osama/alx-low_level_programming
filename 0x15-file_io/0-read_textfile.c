a3cd50642b9a749280842e3e06a30fed19754dc3
Abdo8Osama <abdo.osama301@yahoo.com>
2022-11-01 02:19:28 +0200

    chore : create files.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,48 @@
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/uio.h>
+#include <unistd.h>
 #include <fcntl.h>
-#include "main.h"
+#include <stdlib.h>
 
 /**
- * read_textfile - reads text file and prints it
- *@letters: number of letters should read and print 
- *@filename: file name to read
- *
- *Return: number of letters it could read and print
+ * read_textfile - A function that reads a text file and prints
+ * to the POSIX STDOUT
+ * @filename: The filename to open
+ * @letters: The number of letters to read and print
+ * Return: The number of letters read and printed, or 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int printed = 0;
-	char buf[letters+1];
-	
-	if(filename == NULL)
+	int fdo, fdr, fdw;
+	char *temp;
+
+	if (filename == NULL)
 		return (0);
-	
-	fd = open(filename, O_RDONLY);
-	
-	if(fd == -1)
+
+	temp = malloc(sizeof(char) * letters);
+	if (temp == NULL)
 		return (0);
-	
-	read(fd, buf, letters);
-	buf[letters] = '\0';
-	while(*buf)
+
+	fdo = open(filename, O_RDONLY);
+	if (fdo < 0)
 	{
-		printed += _putchar(*buf);
-		buf++;
+		free(temp);
+		return (0);
 	}
-	
-	close(fd);
-	
-	return printed;
+
+	fdr = read(fdo, temp, letters);
+	if (fdr < 0)
+	{
+		free(temp);
+		return (0);
+	}
+
+	fdw = write(STDOUT_FILENO, temp, fdr);
+	free(temp);
+	close(fdo);
+
+	if (fdw < 0)
+		return (0);
+	return ((ssize_t)fdw);
 }
